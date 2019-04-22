78f3b1f1dba27d47ce78c07ab1606a744802917e
yuki <1822234475@qq.com>
2019-04-22 10:02:02 +0800

    写注释

--- sunxuefeng-common/src/main/java/com/sunxuefeng/common/utils/CollectionUtil.java
+++ sunxuefeng-common/src/main/java/com/sunxuefeng/common/utils/CollectionUtil.java
@@ -6,9 +6,11 @@ import java.util.Map;
 public class CollectionUtil {
 	//方法1：判断List或Set集合是否有元素，对象为空或对象中没有元素都算没值 (5分)
 	public boolean notEmptyListOrSet(Collection c){
+		//判断集合是否为空
 		if(c.isEmpty()){
 			return false;
 		}else{
+			//判断集合是否空值
 			if(c.contains(null)){
 				return false;
 			}
@@ -17,9 +19,11 @@ public class CollectionUtil {
 	}
 	//方法2：判断Map集合是否有元素，对象为空或对象中没有元素都算没值 (5分)
 	public boolean notEmptyOfMap(Map map){
+		//判断集合是否为空
 		if(map.isEmpty()){
 			return false;
 		}else{
+			//判断集合是否空值
 			if(map.containsKey(null)){
 				return false;
 			}
--- sunxuefeng-common/src/main/java/com/sunxuefeng/common/utils/StringUtil.java
+++ sunxuefeng-common/src/main/java/com/sunxuefeng/common/utils/StringUtil.java
@@ -4,9 +4,11 @@ public class StringUtil {
 	// 方法1：判断源字符串是否有值，空引号(空白字符串)也算没值 (5分)
 	public boolean hasLength(String src) {
 		// 实现代码
-		if(src==null){
+		// 判断是否为空如果为空直接返回false
+		if (src == null) {
 			return false;
 		}
+		// 和空串作比较
 		if ("".equals(src) || src.length() == 0) {
 			return false;
 		}
@@ -16,9 +18,11 @@ public class StringUtil {
 	// 方法2：判断源字符串是否有值，空引号(空白字符串)和空格也算没值 (5分)
 	public boolean hasText(String src) {
 		// 实现代码
-		if(src==null){
+		// 判断是否为空如果为空直接返回false
+		if (src == null) {
 			return false;
 		}
+		// 去掉空格和空串作比较
 		if ("".equals(src.trim())) {
 			return false;
 		}
@@ -28,6 +32,7 @@ public class StringUtil {
 	// 方法3：判断是否为手机号码 (5分)
 	public boolean isMobile(String src) {
 		// 实现代码
+		// 利用正则判断手机号
 		String reg = "^1[3578]\\d{9}$";
 		if (src.matches(reg)) {
 			return true;
@@ -38,6 +43,7 @@ public class StringUtil {
 	// 方法4：判断是否为邮箱 (5分)
 	public boolean isEmail(String src) {
 		// 实现代码
+		// 利用正则判断邮箱
 		String reg = "^\\w+@\\w+(.com|.cn|.com.cn)$";
 		if (src.matches(reg)) {
 			return true;
@@ -51,11 +57,13 @@ public class StringUtil {
 		char[] arr = src.toCharArray();
 		char[] newarr = new char[src.length()];
 		int j = arr.length - 1;
+		// 倒叙赋值
 		for (int i = 0; i < arr.length; i++) {
 			if (j >= 0) {
 				newarr[j--] = arr[i];
 			}
 		}
+		// 拼接字符串
 		String string = "";
 		for (char c : newarr) {
 			string += c;
